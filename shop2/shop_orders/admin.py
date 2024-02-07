70f480ea89c6ef11217debd913d5e29ad3a95030
AmirAJ <141641836+Amir-AJ-PRO@users.noreply.github.com>
2024-02-07 13:44:45 +0330

    order item

--- shop2/shop_orders/admin.py
+++ shop2/shop_orders/admin.py
@@ -7,6 +7,16 @@ class OrderItemInLine(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ('product',)
 
+    def get_queryset(self, request):
+        if request.user.is_superuser:
+            return super().get_queryset(request)
+        return super().get_queryset(request).filter(product__storeuser=request.user)
+       
+    def save_model(self, request, obj, form, change):
+        obj.product.storeuser = request.user
+        super().save_model(request, obj, form, change)
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'jcreated', 'paid', 'closed')
